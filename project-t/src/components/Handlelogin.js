d62b3be0eac40310d2236d66bb5666bd08fa750f
ankit10000 <ankitjangid9197@gmail.com>
2022-06-22 23:03:19 +0530

    Ankit's-Branch:change in email validation and error show and css include

--- project-t/src/components/Handlelogin.js
+++ project-t/src/components/Handlelogin.js
@@ -1,28 +1,71 @@
-import React from 'react'
+// import React from 'react'
+
+import React, {useState} from 'react'
 import logo from "../img/logo.png"
 import logo1 from "../img/passlogo.png"
 
 export default function Handlelogin() {
+  const regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+  const [email, setEmail] = useState(''); 
+
+  const [error, setError] = useState(null);
+  const [error1, setError1] = useState(null);
+  const [error2, setError2] = useState('');
+
+
+
+
+  const checkEmail=(e)=>{
+    setEmail(e.target.value);
+    if(regex.test(email)===false){
+      setTimeout(() => {
+        setError(null)
+      }, 1500);
+      setError('Please enter valide email')
+    }
+    else{
+        // setError('');
+        setTimeout(() => {
+          setError1(null)
+        }, 2000);
+        setError1('Verified email');
+      }
+  }
+
+
+  const onSubmit=()=>{   
+    if(email!==""){
+      alert("Thankyou Mr/Miss " + email)
+    }
+    else{
+      
+      setError2("Email can't blank");
+    }
+  }
+ 
   return (
     <><div className="container">
         <div className="outer">
             <h1><span>Sign in</span></h1>
             <span id='span'>Login to manage your account</span>
-            <form action="sada.com" method="post">
+            {/* <form> */}
             <nav className="navbar">
                 <div className="icoemail">
                   <img id='logo' height="20px" width="22px" src={logo} alt="" />
                   <label htmlFor='email'></label>
-                  <input type="text" maxLength="40" placeholder='Enter your email' id='email' />
+                  <input type="text" maxLength="40" placeholder='Enter your email' id='email' onChange={checkEmail} required/>
                 </div>
+                  <span id='error'>{error}</span>
+                  <span id='error1'>{error1}</span>
+                  <span id='error2'>{error2}</span>
                 <div className="passico">
                   <img id='logo1' height="20px" width="22px" src={logo1} alt="" />
                   <label htmlFor='password'></label>
-                  <input minLength="6" maxLength="10" type="password" id='pass' />
+                  <input minLength="6" maxLength="10" placeholder='Enter you password' type="password" id='pass' required/>
                 </div>
-                <button type="submit">Submit</button>
+                <button className='btn' type="submit" onClick={onSubmit}>Submit</button>
             </nav>
-            </form>
+            {/* </form> */}
         </div>
     </div>
     </>
