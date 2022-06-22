8988dc71beba94b100972336bd23278f614d0359
ankit10000 <ankitjangid9197@gmail.com>
2022-06-22 14:18:21 +0530

    Ankit's-Branch: set css for nav

--- project-t/src/components/Handlelogin.js
+++ project-t/src/components/Handlelogin.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import logo from "../img/logo.png"
+import logo1 from "../img/passlogo.png"
 
-export default function Handlelogin(props) {
+export default function Handlelogin() {
   return (
     <><div className="container">
         <div className="outer">
             <h1><span>Sign in</span></h1>
             <span id='span'>Login to manage your account</span>
+            <form action="sada.com" method="post">
             <nav className="navbar">
                 <div className="icoemail">
                   <img id='logo' height="20px" width="22px" src={logo} alt="" />
                   <label htmlFor='email'></label>
                   <input type="text" maxLength="40" placeholder='Enter your email' id='email' />
                 </div>
-                <label htmlFor='password'></label>
-                <input type="password" id='pass' />
+                <div className="passico">
+                  <img id='logo1' height="20px" width="22px" src={logo1} alt="" />
+                  <label htmlFor='password'></label>
+                  <input minLength="6" maxLength="10" type="password" id='pass' />
+                </div>
+                <button type="submit">Submit</button>
             </nav>
+            </form>
         </div>
     </div>
     </>
